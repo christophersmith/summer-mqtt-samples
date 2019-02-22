546d5b879022d198691e11c456b8b4407c245a1b
Christopher Smith <christophersmith@users.noreply.github.com>
2019-02-22 16:18:43 -0500

    added a Published Event Listener to log when a message is published.

---
+++ summer-mqtt-samples-paho-async-publish-only/src/main/java/com/github/christophersmith/summer/mqtt/samples/paho/pubonly/service/ApplicationMessagePublishedEventListenerService.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Christopher Smith - https://github.com/christophersmith
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.github.christophersmith.summer.mqtt.samples.paho.pubonly.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Service;
+
+import com.github.christophersmith.summer.mqtt.core.event.MqttMessagePublishedEvent;
+
+@Service
+public class ApplicationMessagePublishedEventListenerService
+    implements ApplicationListener<MqttMessagePublishedEvent>
+{
+    private static final Logger LOG = LoggerFactory
+        .getLogger(ApplicationMessagePublishedEventListenerService.class);
+
+    @Override
+    public void onApplicationEvent(MqttMessagePublishedEvent event)
+    {
+        LOG.info(
+            String.format("Published Event - Client ID: %s, Correlation ID: %s, Message ID: %s",
+                event.getClientId(), event.getCorrelationId(),
+                String.valueOf(event.getMessageIdentifier())));
+
+    }
+}
